643dfc5758043db609c4ce6515444dc2434cfaee
Ma Yanlong <l.luffy.river@gmail.com>
2016-03-04 00:17:17 +0800

    ..

--- js/index.js
+++ js/index.js
@@ -1,109 +1,139 @@
-var sh = angular.module('sketch', []);
+var sketch = angular.module('sketch',[]);
+sketch.controller('sketchController', ['$scope',function($scope){
+	$scope.wh = [600,600];
+	$scope.tool = 'rect';
+	$scope.tools = {
+		'画线':'line',
+		'画圆':'arc',
+		'矩形':'rect',
+		'铅笔':'pen',
+		'橡皮':'erase',
+		'选择':'select'
+	};
+	$scope.csState = {
+		fillStyle:'#000000',
+		strokeStyle:'#000000',
+		lineWidth:1,
+		style:'stroke'
+	};
 
-sh.controller('shController', ['$scope', function($scope){
-
-	$scope.canvasWH = {width:600,height:600};
-	$scope.canvas = document.querySelector('#canvas');
-	$scope.ctx = $scope.canvas.getContext('2d');
-
-	$scope.tools = ['line','rect','arc','pen','erase','select'];
-	$scope.state = {
-		tool:'line',
-		fillStyle:'#000',
-		strokeStyle:'#000',
-		type:'stroke',
-		lineWidth:'1',
+	$scope.setStyle = function(s) {
+		$scope.csState.style = s;
 	}
-	$scope.history = [];
-
-	$scope.redrawLastHistory = function (ctx) {
-		if( $scope.history.length )
-			ctx.putImageData($scope.history[$scope.history.length-1],0,0);	
+	$scope.setTool = function(s) {
+		$scope.tool = s;
 	}
-	$scope.pen = function (ctx,el) {
-		ctx.beginPath();
-		ctx.moveTo(el.offsetX,el.offsetY);
-		$scope.canvas.onmousemove = function(ev){
-			ctx.lineTo(ev.offsetX,ev.offsetY);
-			ctx.stroke();
+	$scope.newSketch = function(ev){
+		if(history.length){
+			if( confirm('是否保存') ){
+        ev.srcElement.href=canvas.toDataURL();
+        ev.srcElement.download = 'mypic.png';
+			}else{
+        ev.preventDefault();
+      }
+		}
+		clearCanvas();
+	}
+	$scope.save = function(ev){
+		if(history.length){
+			ev.srcElement.href=canvas.toDataURL();
+			ev.srcElement.download = 'mypic.png';
+		}else{
+			alert('空画布');
 		}
 	}
-	$scope.line = function (ctx,el) {
-		$scope.canvas.onmousemove  = function (ev) {
-			ctx.clearRect(0,0,600,600);
-			$scope.redrawLastHistory(ctx);
-			ctx.beginPath();	
-			ctx.moveTo(el.offsetX,el.offsetY);
-			ctx.lineTo(ev.offsetX,ev.offsetY);	
-			ctx.stroke();
+
+  $scope.undo = function (ev) {
+    ev.stopPropagation();
+    clearCanvas();
+    history.pop();
+    if(history.length){
+      ctx.putImageData(history[history.length-1],0,0);
 		}
+  }
+
+	var
+	canvas = document.querySelector('#canvas'),
+	ctx = canvas.getContext('2d'),
+  history = [];
+
+	var clearCanvas = function() {
+		ctx.clearRect(0,0,$scope.wh[0],$scope.wh[1]);
 	}
-	$scope.rect = function (ctx,el) {
-		$scope.canvas.onmousemove = function (ev) {
-			ctx.clearRect(0,0,600,600);
-			$scope.redrawLastHistory(ctx);
-			if($scope.state.type=='fill'){
-				ctx.fillRect(el.offsetX,el.offsetY,ev.offsetX-el.offsetX,ev.offsetY-el.offsetY);		
-			}else{
-				ctx.strokeRect(el.offsetX,el.offsetY,ev.offsetX-el.offsetX,ev.offsetY-el.offsetY);		
-			}
-		}	
-	}
-	$scope.arc = function (ctx,el) {
-		$scope.canvas.onmousemove = function (ev) {
-			ctx.clearRect(0,0,600,600);
-			$scope.redrawLastHistory(ctx);
-			ctx.beginPath();
-			ctx.arc(el.offsetX,el.offsetY,Math.abs(ev.offsetX-el.offsetX),0,Math.PI*2);		
-			if($scope.state.type == 'fill'){
-				ctx.fill();
-			}else{
+  var redrawLastHistory = function () {
+    if( history.length ){
+      ctx.putImageData(history[history.length-1],0,0);
+    }
+  }
+	var setmousemove = {
+		line:function (e) {
+			canvas.onmousemove = function(ev) {
+				clearCanvas();
+        redrawLastHistory();
+				ctx.beginPath();
+				ctx.moveTo(e.offsetX,e.offsetY);
+				ctx.lineTo(ev.offsetX,ev.offsetY);
 				ctx.stroke();
 			}
-		}		
-	}
-	$scope.erase  = function(ctx,el){
-		$scope.canvas.onmousemove  = function (ev) {
-			ctx.clearRect(ev.offsetX,ev.offsetY,20,20);
+		},
+		rect:function(e) {
+			canvas.onmousemove = function(ev) {
+				clearCanvas();
+        redrawLastHistory();
+				ctx.beginPath();
+				var w = ev.offsetX - e.offsetX;
+				var h = ev.offsetY - e.offsetY;
+				if($scope.csState.style == 'fill'){
+					ctx.fillRect(e.offsetX-0.5,e.offsetY-0.5,w,h);
+				}else{
+					ctx.strokeRect(e.offsetX-0.5,e.offsetY-0.5,w,h);
+				}
+			}
+		},
+		arc:function(e) {
+			canvas.onmousemove = function(ev) {
+				clearCanvas();
+        redrawLastHistory();
+				ctx.beginPath();
+				var r = Math.abs(e.offsetX - ev.offsetX);
+				ctx.arc(e.offsetX,e.offsetY,r,0,Math.PI*2);
+				if($scope.csState.style == 'fill'){
+					ctx.fill();
+				}else{
+					ctx.stroke();
+				}
+			}
+		},
+		erase:function(e) {
+			canvas.onmousemove = function(ev){
+				ctx.clearRect(ev.offsetX,ev.offsetY,20,20);
+			}
+		},
+		pen:function(e) {
+			ctx.beginPath();
+			ctx.moveTo(e.offsetX,e.offsetY);
+			canvas.onmousemove = function(ev) {
+				clearCanvas();
+        redrawLastHistory();
+				ctx.lineTo(ev.offsetX,ev.offsetY);
+				ctx.stroke();
+			}
+		},
+		select:function(e) {
+			console.log('select');
 		}
-	}
-	$scope.select = function (ctx,el) {
-		$scope.canvas.onmousemove = function (ev) {
-			
-		}		
 	}
 
-	$scope.mousedown = function(el){
-		$scope.ctx.fillStyle = $scope.state.fillStyle;
-		$scope.ctx.strokeStyle = $scope.state.strokeStyle;
-		$scope.ctx.lineWidth  = $scope.state.lineWidth;
-		$scope[$scope.state.tool]($scope.ctx,el);
-		document.onmouseup = function(){
-			canvas.onmouseup = null;
+	canvas.onmousedown = function(e) {
+		ctx.strokeStyle = $scope.csState.strokeStyle;
+		ctx.fillStyle   = $scope.csState.fillStyle;
+		ctx.lineWidth   = $scope.csState.lineWidth;
+
+		setmousemove[$scope.tool](e);
+		document.onmouseup = function() {
 			canvas.onmousemove = null;
-			$scope.history.push($scope.ctx.getImageData(0, 0, 600, 600));
+      history.push( ctx.getImageData(0, 0, $scope.wh[0], $scope.wh[1]) );
 		}
 	}
-	$scope.undo = function(ev){
-		ev.stopPropagation();
-		$scope.history.pop();	
-		$scope.ctx.clearRect(0,0,600,600);
-		if($scope.history.length){
-			$scope.redrawLastHistory($scope.ctx);
-		}else{
-			alert('no history');
-		}
-	}
-	$scope.save = function () {
-		location.href=($scope.canvas.toDataURL().replace("data:image/png","data:stream/octet"));
-	}
-	$scope.newCanvas = function () {
-		if($scope.history.length){
-			if(confirm('是否保存当前图像')){
-				$scope.save();
-			}
-		}
-		$scope.ctx.clearRect(0,0,600,600);
-		$scope.history = [];
-	}
-}]);
+
+}])
